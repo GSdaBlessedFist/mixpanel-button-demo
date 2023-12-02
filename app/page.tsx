92fc50ae9c812e503c098b694047b4dab2493fbd
GSdaBlessedFist <gsdablessedfist@gmail.com>
2023-12-02 17:04:11 -0600

    1)got rid of grid system...2) started on the info section

--- app/page.tsx
+++ app/page.tsx
@@ -3,23 +3,53 @@ import Image from 'next/image';
 import Navbar from "../components/Navbar";
 import styles from "./styles/mainContainer.module.scss";
 import SVGButton from "../components/SVGButton";
+import ResultGroup from "../components/ResultGroup";
 import { useEffect, useRef, useState } from 'react';
 
 type HomeProps = {};
 
 export default function Home(props: HomeProps) {
 
+  const findings = [
+    {
+      query: "How many people have visited?",
+      result: 50
+    },{
+      query: "How many times repeated?",
+      result: 85
+    },{
+      query: "How many visited a few more?",
+      result: 150
+    },{
+      query: "Which witches visited?",
+      result: 50
+    },{
+      query: "How many visited a few more?",
+      result: 150
+    },{
+      query: "Which witches visited?",
+      result: 50
+    }
+  ]
   useEffect(() => {
-
-
-
   }, []);
 
   return (
     <>
       <Navbar />
       <div className={styles.mainContainer}>
-        
+        <div className={styles.svgButtonContainer}>
+          <SVGButton/>
+        </div>
+        <div className={styles.dashboardContainer}>
+          {findings.map((finding,i)=>(
+             <div key={i}>
+                <ResultGroup finding={finding}/>
+             </div>
+              
+          ))}
+          {/* <ResultGroup finding={findings[0]}/> */}
+        </div>
       </div>
 
 
---
+++ components/ResultGroup.jsx
@@ -0,0 +1,36 @@
+"use client";
+import { useEffect } from "react";
+import styles from "./resultGroup.module.scss";
+
+function ResultGroup({ finding }) {
+    useEffect(() => {
+        console.log(finding.query)
+    }, [])
+
+    return (<>
+        <div className={styles.resultGroup}>
+            <NumberDisplay result={finding.result}/>
+            <QueryDisplay query={finding.query} />
+        </div>
+    </>);
+}
+
+
+function NumberDisplay({result}) {
+    return (<>
+        <div className={styles.numberDisplay}>
+            {result}
+        </div>
+    </>)
+}
+
+function QueryDisplay({ query }) {
+    return (<>
+        <div className={styles.queryDisplay}>
+            {query}
+        </div>
+    </>)
+}
+
+
+export default ResultGroup;
