bae324dd099fcbb7385e38e198165bfb43b73349
GSdaBlessedFist <gsdablessedfist@gmail.com>
2023-12-04 17:01:00 -0600

    cleaned up dashboard reminense...NEXT:: logic for event types

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,8 @@ import Image from 'next/image';
 import Navbar from "../components/Navbar";
 import styles from "./styles/mainContainer.module.scss";
 import SVGButton from "../components/SVGButton";
-import ResultGroup from "../components/ResultGroup";
-import DashboardTab from "../components/DashboardTab";
 import { useEffect, useRef, useState } from 'react';
 import MixpanelTracking from '@/service/mixpanel';
-import axios from 'axios';
 
 type HomeProps = {};
 
@@ -17,40 +14,12 @@ export default function Home(props: HomeProps) {
   const [hasTrackedPageView, setHasTrackedPageView] = useState(false);
   const [mixpanelData, setMixpanelData] = useState(null);
 
-  const findings = [
-    {
-      query: "How many people have visited?",
-      result: 50
-    }, {
-      query: "How many times repeated?",
-      result: 85
-    }, {
-      query: "How many visited a few more?",
-      result: 150
-    }, {
-      query: "Which witches visited?",
-      result: 50
-    }, {
-      query: "How many visited a few more?",
-      result: 150
-    }, {
-      query: "Which witches visited?",
-      result: 50
-    }
-  ]
+  
 
   useEffect(() => {
     if (!hasTrackedPageView) {
       MixpanelTracking.getInstance().pageViewed();
       setHasTrackedPageView(true);
-
-      
-
-      
-      
-  
-      
-
     }
 
   }, [hasTrackedPageView]);
@@ -62,15 +31,7 @@ export default function Home(props: HomeProps) {
         <div className={styles.svgButtonContainer}>
           <SVGButton />
         </div>
-        <div className={styles.dashboardContainer}>
-          {findings.map((finding, i) => (
-            <div key={i}>
-              <ResultGroup finding={finding} />
-            </div>
-
-          ))}
-          <DashboardTab />
-        </div>
+        
 
       </div>
 
