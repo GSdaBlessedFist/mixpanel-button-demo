95c0468751d08961240d57ca1c59364ea5a1d801
GSdaBlessedFist <gsdablessedfist@gmail.com>
2023-12-04 21:04:02 -0600

    grid mistake

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 import Image from 'next/image';
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 import styles from "./styles/mainContainer.module.scss";
 import SVGButton from "../components/SVGButton";
 import { useEffect, useRef, useState } from 'react';
 import MixpanelTracking from '@/service/mixpanel';
+import Link from 'next/link';
 
 type HomeProps = {};
 
@@ -14,7 +16,7 @@ export default function Home(props: HomeProps) {
   const [hasTrackedPageView, setHasTrackedPageView] = useState(false);
   const [mixpanelData, setMixpanelData] = useState(null);
 
-  
+
 
   useEffect(() => {
     if (!hasTrackedPageView) {
@@ -26,16 +28,31 @@ export default function Home(props: HomeProps) {
 
   return (
     <>
-      <Navbar />
-      <div className={styles.mainContainer}>
-        <div className={styles.svgButtonContainer}>
-          <SVGButton />
-        </div>
-        
+      <Grid >
+        <Navbar />
 
-      </div>
+        <div className={styles.mainContainer}>
+          <div className={styles.dashboardLink}>
+            <a href="https://mixpanel.com/p/TFy5N9adR8zjK1qQ2R4v5F"
+              className='text-primary'>check out the dashboard</a>
+          </div>
+          <div className={styles.svgButtonContainer}>
+            <SVGButton />
+          </div>
+        </div>
+
+        <Footer />
+      </Grid>
+
 
 
     </>
   );
+}
+
+function Grid({children} ) {
+  return (<>
+    <div className="w-full lg:w-2/3 grid grid-rows-[85px_1fr_65px] h-screen ">{children}</div>
+  </>
+  )
 }
---
+++ components/Footer.jsx
@@ -0,0 +1,12 @@
+import styles from "../components/footer.module.scss";
+
+
+function Footer() {
+    return (
+        <div className={styles.footer}>
+            Enter
+        </div>
+    );
+}
+
+export default Footer;
