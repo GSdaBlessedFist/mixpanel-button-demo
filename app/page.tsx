75228b74722ecb0ce6a5d779654ee3bdb7fbae46
GSdaBlessedFist <gsdablessedfist@gmail.com>
2023-12-04 21:26:03 -0600

    got a hold of width issues in nav and footer

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import styles from "./styles/mainContainer.module.scss";
 import SVGButton from "../components/SVGButton";
 import { useEffect, useRef, useState } from 'react';
 import MixpanelTracking from '@/service/mixpanel';
-import Link from 'next/link';
 
 type HomeProps = {};
 
@@ -16,7 +15,7 @@ export default function Home(props: HomeProps) {
   const [hasTrackedPageView, setHasTrackedPageView] = useState(false);
   const [mixpanelData, setMixpanelData] = useState(null);
 
-
+  
 
   useEffect(() => {
     if (!hasTrackedPageView) {
@@ -28,31 +27,18 @@ export default function Home(props: HomeProps) {
 
   return (
     <>
-      <Grid >
-        <Navbar />
-
-        <div className={styles.mainContainer}>
-          <div className={styles.dashboardLink}>
-            <a href="https://mixpanel.com/p/TFy5N9adR8zjK1qQ2R4v5F"
-              className='text-primary'>check out the dashboard</a>
-          </div>
-          <div className={styles.svgButtonContainer}>
-            <SVGButton />
-          </div>
+      <div className="sm:w-full flex flex-col h-screen">
+      <Navbar />
+      <div className={styles.mainContainer}>
+        <div className={styles.svgButtonContainer}>
+          <SVGButton />
         </div>
+        
 
-        <Footer />
-      </Grid>
-
-
+      </div>
+      <Footer/>
+      </div>
 
     </>
   );
-}
-
-function Grid({children} ) {
-  return (<>
-    <div className="w-full lg:w-2/3 grid grid-rows-[85px_1fr_65px] h-screen ">{children}</div>
-  </>
-  )
 }
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,8 @@ import styles from "../components/footer.module.scss";
 function Footer() {
     return (
         <div className={styles.footer}>
-            Enter
+            <div>by Jason Zamora</div>
+            <div>Thank you for your consideration.</div>
         </div>
     );
 }
