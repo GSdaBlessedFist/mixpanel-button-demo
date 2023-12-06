1d53e382fb2a590845d0ca6712782493c06aa2d5
GSdaBlessedFist <gsdablessedfist@gmail.com>
2023-12-06 14:13:56 -0600

    adjusted navbar/footer info

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ import styles from "../components/footer.module.scss";
 function Footer() {
     return (
         <div className={styles.footer}>
-            <div>by Jason Zamora</div>
+            
             <div>Thank you for your consideration.</div>
         </div>
     );
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,8 +7,9 @@ function Navbar() {
             <div className={styles.title}>Mixpanel <span className="relative text-lg top-2 opacity-50">demo</span></div>
             <div className={styles.navMenu}>
                 
-                <div className="menuItem">
-                    <Link href="/resume" target="_blank">
+                <div className={styles.menuItem}>
+                    <span className="hidden lg:block">Jason Zamora's</span>
+                    <Link href="/resume" target="_blank" className=" text-primary-light hover:text-white">
                         resum&eacute;
                     </Link>
                 </div>
