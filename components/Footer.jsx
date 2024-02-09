e2fbd98f33558581d860b32ae506769190fb1976
GSdaBlessedFist <gsdablessedfist@gmail.com>
2024-02-09 10:53:52 -0600

    1)updated resume...2)added github link

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,9 @@ import styles from "../components/footer.module.scss";
 function Footer() {
     return (
         <div className={styles.footer}>
-            
+            <div>
+                <a href="https://github.com/GSdaBlessedFist/mixpanel-button-demo" target="blank" className="text-primary-light hover:text-white">[github]</a>
+            </div>
             <div>Thank you for your consideration.</div>
         </div>
     );
